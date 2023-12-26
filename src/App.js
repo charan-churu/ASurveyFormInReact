d5253d45d0d4c432f6d33c50b26e9dfaf20db434
charan-churu <92526595+charan-churu@users.noreply.github.com>
2023-12-26 19:53:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ASurveyForm from './components/ASurveyForm';
+function App() {
+  return (
+    <div className="App">
+      <ASurveyForm/>
+    </div>
+  );
+}
+
+export default App;
